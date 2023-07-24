7660e5f6f6d2ac0242e5b2a3144de01c618f3aba
taido <dophutai.2298@gmail.com>
2023-07-25 01:14:19 +0700

    hardcode login

--- controllers/authController.js
+++ controllers/authController.js
@@ -13,7 +13,28 @@ const login = (req, res) => {
       error: "You need to enter all the information",
     });
   }
-  const user = yaml.load(fs.readFileSync(process.env.URL_YAML));
+  // const user = yaml.load(fs.readFileSync(process.env.URL_YAML));
+  // const passwordMd5 = md5(req.body.password);
+  const user = [{
+    "id": 1,
+    "username":"user",
+    "password": "827ccb0eea8a706c4c34a16891f84e7b"
+  },
+  {
+    "id": 2,
+    "username":"user1",
+    "password": "e10adc3949ba59abbe56e057f20f883e"
+  },
+  {
+    "id": 3,
+    "username":"user2",
+    "password": "fcea920f7412b5da7be0cf42b8c93759"
+  },
+  {
+    "id": 4,
+    "username":"user3",
+    "password": "25d55ad283aa400af464c76d713c07ad"
+  }]
   const passwordMd5 = md5(req.body.password);
   const countUser = user.length;
   for (let i = 0; i < countUser; i++) {
