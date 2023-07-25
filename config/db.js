d6fb233469bd6b3345bd1a0c47e624b26472c9e3
knguyen2 <knguyen2@bluecatnetworks.com>
2023-07-25 09:25:14 +0700

    handle Disconnect

--- config/db.js
+++ config/db.js
@@ -10,11 +10,34 @@ const connection = mysql.createConnection({
   timezone: process.env.TM_ZONE,
 });
 
-connection.connect((err) => {
-  if (!err) {
+// connection.connect((err) => {
+//   if (!err) {
+//     console.log("database connection successful");
+//   } else {
+//     console.log("Database connection failed");
+//   }
+// });
+
+function handleDisconnect() {
+  connection.connect(function(err) {              // The server is either down
+    if(err) {                                     // or restarting (takes a while sometimes).
+      console.log('error when connecting to db:', err);
+      setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+    }     
+    else if (!err) {
     console.log("database connection successful");
-  } else {
-    console.log("Database connection failed");
-  }
-});
-module.exports = connection;
+  }                                 // to avoid a hot loop, and to allow our node script to
+  });                                     // process asynchronous requests in the meantime.
+                                          // If you're also serving http, display a 503 error.
+  connection.on('error', function(err) {
+    console.log('db error', err);
+    if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+      handleDisconnect();                         // lost due to either server restart, or a
+    } else {                                      // connnection idle timeout (the wait_timeout
+      throw err;                                  // server variable configures this)
+    }
+  });
+}
+
+handleDisconnect();
+module.exports = handleDisconnect;
