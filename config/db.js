6492046b490b1b708047c21463defa06d130caf2
knguyen2 <knguyen2@bluecatnetworks.com>
2023-07-25 11:31:01 +0700

    hardcode Connect to DB

--- config/db.js
+++ config/db.js
@@ -2,42 +2,19 @@ const mysql = require("mysql");
 const dotenv = require("dotenv");
 dotenv.config();
 const connection = mysql.createConnection({
-  port: process.env.DB_PORT,
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
-  timezone: process.env.TM_ZONE,
+  port: 3306,
+  host: "bavtrtdfiype2lknyeug-mysql.services.clever-cloud.com",
+  user: "uflu92a6qgqpypl1",
+  password: "fcBcmi9Xbq76Bm6VMtTI",
+  database: "bavtrtdfiype2lknyeug",
+  timezone: "UTC",
 });
 
-// connection.connect((err) => {
-//   if (!err) {
-//     console.log("database connection successful");
-//   } else {
-//     console.log("Database connection failed");
-//   }
-// });
-
-function handleDisconnect() {
-  connection.connect(function(err) {              // The server is either down
-    if(err) {                                     // or restarting (takes a while sometimes).
-      console.log('error when connecting to db:', err);
-      setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
-    }     
-    else if (!err) {
+connection.connect((err) => {
+  if (!err) {
     console.log("database connection successful");
-  }                                 // to avoid a hot loop, and to allow our node script to
-  });                                     // process asynchronous requests in the meantime.
-                                          // If you're also serving http, display a 503 error.
-  connection.on('error', function(err) {
-    console.log('db error', err);
-    if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
-      handleDisconnect();                         // lost due to either server restart, or a
-    } else {                                      // connnection idle timeout (the wait_timeout
-      throw err;                                  // server variable configures this)
-    }
-  });
-}
-
-handleDisconnect();
-module.exports = handleDisconnect;
+  } else {
+    console.log("Database connection failed");
+  }
+});
+module.exports = connection;
